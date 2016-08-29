b66b7d84a455268e91738d129adffcf4037f8a7a
vh <all-test@users.noreply.github.com>
2016-08-29 18:12:44 +0200

    pokus s adresama

--- test-resteasy-undertow/src/main/java/TestUndertow.java
+++ test-resteasy-undertow/src/main/java/TestUndertow.java
@@ -1,11 +1,16 @@
+import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
 
@@ -24,8 +29,8 @@ public class TestUndertow {
 		System.out.println("http://localhost:8001/root/test");
 		server.deploy(MyApp.class, "root");
 		
-		System.out.println("http://localhost:8001/test");
-		server.deploy(MyApp.class, "/");
+		//System.out.println("http://localhost:8001/test");
+		server.deploy(IndexApp.class, "/");
 		
 		
 		//server pobezi minutu, pak to ukoncm
@@ -55,5 +60,35 @@ public class TestUndertow {
 			return classes;
 		}
 	}
+	
+	
+	@Path("/")
+	public static class IndexResource {
+		@GET
+		@Path("")
+		public Response getIndex(@Context UriInfo uriInfo) {
+			System.out.println("index");
+			URI uri = uriInfo.getAbsolutePathBuilder().path("index.html").build();
+			return Response.temporaryRedirect(uri).build();
+		}
+		
+		@GET
+		@Produces("text/plain")
+		@Path("/{name: [a-zA-Z_0-9]*\\.html}")
+		public String getHtmlFile(@PathParam("name") String name) {
+			System.out.println("file: "+name);
+			return "file: "+name;
+		}		
+	}
+
+	@ApplicationPath("/")
+	public static class IndexApp extends Application {
+		@Override
+		public Set<Class<?>> getClasses() {
+			HashSet<Class<?>> classes = new HashSet<Class<?>>();
+			classes.add(IndexResource.class);
+			return classes;
+		}
+	}	
 
 }
